84e26a3595f19e766c4d98a605fddc83dfd2ec79
yesStan <getiffive@gmail.com>
2022-07-22 15:07:42 +0300

    refactor

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// heroku ps:scale web=0  
 const { get } = require('http');
 const dayjs = require('dayjs')
 
@@ -22,6 +23,7 @@ const bot = new TelegramBot(token, { polling: true });
 app.set('port', (process.env.PORT || 6000));
 
 //For avoiding Heroku $PORT error
+
 app.get('/', function (request, response) {
     let result = 'App is running'
     response.send(result);
@@ -38,7 +40,7 @@ bot.on('message', async (msg) => {
         // reply_to_message_id: msg.message_id,
         reply_markup: {
             resize_keyboard: true,
-            keyboard: [['Kiev'], ['Dnipro']]
+            keyboard: [['Weather'], ['Currency']]
         }
     };
 
@@ -46,119 +48,101 @@ bot.on('message', async (msg) => {
         bot.sendMessage(msg.chat.id, "choose from the menu", opts);
     }
 
-    // const choose = ((city) => {
-    //     const opts = {
-    //         reply_markup: {
-    //             keyboard: [['3h'], ['6h']]
-    //         }
-    //     };
-    //     return city === 'Kiev' || city === 'Dnipro'
-    //         ? bot.sendMessage(msg.chat.id, "alright choose time frame that u wanna display", opts)
-    //         : bot.sendMessage(msg.chat.id, "in process")
-    // })
-    // choose(text)
+    if (text === 'Weather') {
+        const opts = {
+            reply_markup: {
+                keyboard: [['Kiev 3 hours range'], ['Kiev 6 hours range'], ['Prev menu']]
+            }
+        };
+        bot.sendMessage(msg.chat.id, "alright choose time frame that u wanna display", opts)
 
-    if (text === 'Kiev') {
-        const opts = {
-            reply_markup: {
-                keyboard: [['Kiev 3 hours range'], ['Kiev 6 hours range']]
+    }
+
+    if (text === 'Prev menu') {
+        bot.sendMessage(msg.chat.id, 'choose from the menu', opts);
+    }
+
+    const degreeSymbol = (value) => {
+        return value >= 0 ? `+${value} C°`
+            : value < 0
+                ? `-${value} C°`
+                : `${value} C°`
+    }
+    
+    const DEFAULT_TIME_INTERVAL_LIST = ['00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00'];
+    const PREPEARED_REQUEST_PARAMS = {
+        appid: '4468e661cae3911dc87cc649a402ebf1',
+        lat: '50.4501',
+        lon: '30.5234',
+        lang: 'ru',
+        units: 'metric'
+    }
+
+    function getWeatherForecast(params = {}) {
+        return axios.get('https://api.openweathermap.org/data/2.5/forecast?', {
+            params: {
+                ...PREPEARED_REQUEST_PARAMS,
+                ...params
             }
-        };
-        bot.sendMessage(msg.chat.id, "alright choose time frame that u wanna display", opts)
-    } else if (text === 'Dnipro') {
-        const opts = {
-            reply_markup: {
-                keyboard: [['Dnipro 3 hours range'], ['Dnipro 6 hours range']]
+        });
+    }
+
+    function getFormatedData(list = [], timeIntervalList = DEFAULT_TIME_INTERVAL_LIST) {
+        return list.reduce((acc, item) => {
+            const key = item.dt_txt.split(' ')[0];
+            const time = dayjs.unix(item.dt).format('HH:mm') //.split(`/n`)
+            const feelsLike = item.main.feels_like.toFixed();
+            const temp = item.main.temp.toFixed();
+            const weather = item.weather[0].description;
+
+            const forecastData = {
+                [key]: [
+                    ...acc?.[key] || [],
+                    `${time}, ${degreeSymbol(temp)} ощущается как:${degreeSymbol(feelsLike)}, ${weather} `
+                ]
             }
-        };
-        bot.sendMessage(msg.chat.id, "alright choose time frame that u wanna display", opts)
+            const localData = timeIntervalList.includes(time) ? forecastData : {}
+
+            return {
+                ...acc,
+                ...localData
+            }
+        }, {})
+    }
+
+    function getPreparedTemplateData(data = {}) {
+        return Object.entries(data).map(([key, value]) => {
+            const title = dayjs(key).format('dddd, D MMMM')
+            return `${title}: \n\t${value.join('\n\t')}`
+        }).join('\n\n')
     }
 
     if (msg.text === 'Kiev 3 hours range') {
-        // bot.sendMessage(chatId, `Погода в: ${msg.text}`);
         try {
-            const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=50.4501&lon=30.5234&lang=ru&appid=4468e661cae3911dc87cc649a402ebf1&units=metric`);
+            const { data } = await getWeatherForecast();  //const { data } = await getWeatherForecast({lan: '4322', lon : '43222'});
             const { list } = data;
 
-            // {
-            //     dt: 1658091600,
-            //     main: {
-            //       temp: 287.71,
-            //       feels_like: 286.84,
-            //       temp_min: 287.71,
-            //       temp_max: 287.71,
-            //       pressure: 1020,
-            //       sea_level: 1020,
-            //       grnd_level: 1000,
-            //       humidity: 62,
-            //       temp_kf: 0
-            //     },
-            //     weather: [ [Object] ],
-            //     clouds: { all: 34 },
-            //     wind: { speed: 3.09, deg: 280, gust: 7.55 },
-            //     visibility: 10000,
-            //     pop: 0,
-            //     sys: { pod: 'n' },
-            //     dt_txt: '2022-07-17 21:00:00'
-            //   },
-
-            const degreeSymbol = (value) => {
-                return value >= 0 ? `+${value} C°`
-                    : value < 0
-                        ? `-${value} C°`
-                        : `${value} C°`
-            }
-
-            const preparedData = list.reduce((acc, item) => {
-                const key = item.dt_txt.split(' ')[0];
-                const time = dayjs.unix(item.dt).format('HH:mm')
-                const feelsLike = item.main.feels_like.toFixed();
-                const temp = item.main.temp.toFixed();
-                const weather = item.weather[0].description;
-
-                return {
-                    ...acc,
-                    [key]: [
-                        ...acc?.[key] || [],
-                        `${time}, ${degreeSymbol(temp)} ощущается как:${degreeSymbol(feelsLike)}, ${weather} `
-                    ]
-                }
-            }, {})
-
-            const outputData = Object.entries(preparedData).map(([key, value]) => {
-                const title = dayjs(key).format('dddd, D MMMM')
-                return `${title}: \n\t${value.join('\n\t')}`
-            }).join('\n\n')
+            const formatedData = getFormatedData(list)
+            const outputData = getPreparedTemplateData(formatedData)
 
             bot.sendMessage(chatId, `${outputData}`);
-//Dnipro
-            if (msg.text === 'Dnipro 3 hours range') {
-                const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=48.46471&lon=35.0462&lang=ru&appid=4468e661cae3911dc87cc649a402ebf1&units=metric`);
-                const { list } = data;
 
-                const preparedData = list.reduce((acc, item) => {
-                    const key = item.dt_txt.split(' ')[0];
-                    const time = dayjs.unix(item.dt).format('HH:mm')
-                    const feelsLike = item.main.feels_like.toFixed();
-                    const temp = item.main.temp.toFixed();
-                    const weather = item.weather[0].description;
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
-                    return {
-                        ...acc,
-                        [key]: [
-                            ...acc?.[key] || [],
-                            `${time}, ${degreeSymbol(temp)} ощущается как:${degreeSymbol(feelsLike)}, ${weather} `
-                        ]
-                    }
-                }, {})
+    if (msg.text === 'Kiev 6 hours range') {
+        // bot.sendMessage(chatId, `Погода в: ${msg.text}`);
+        try {
+            const { data } = await getWeatherForecast();
+            const { list } = data;
 
-                const outputData = Object.entries(preparedData).map(([key, value]) => {
-                    const title = dayjs(key).format('dddd, D MMMM')
-                    return `${title}: \n\t${value.join('\n\t')}`
-                }).join('\n\n')
+            const formatedData = getFormatedData(list, ['00:00', '06:00', '12:00', '18:00'])
+            const outputData = getPreparedTemplateData(formatedData)
 
-                bot.sendMessage(chatId, `${outputData}`);
-            }
+            bot.sendMessage(chatId, `${outputData}`);
+
         } catch (e) {
             console.log(e);
         }
